4ec0691d55c42e9a5105267fd64d6f02986e9315
ambihelical <ambihelical@users.noreply.github.com>
2015-11-03 12:14:42 -0800

    Fix update_base_packages on Ubuntu < 15.10
    * linux_distribution_version failed to return comparable integer when
      version has 3 sections (e.g. 14.04.03).
    * also cleanup bash comparison operations in script

--- bin/common/linux.sh
+++ bin/common/linux.sh
@@ -9,12 +9,16 @@ linux_distribution_name() {
 	echo $distro
 }
 
-# return the linux distribution as a 4 digit number
+# return the linux distribution as a comparable integer
+# ISSUE: this only works for Ubuntu's numbering convention,
+# Other distributions may not be so readable if the numbers
+# must stay comparable.
 linux_distribution_version() {
 	version="0000"
 	if hash lsb_release 2> /dev/null; then
 		 release=( $(lsb_release -ds) )
 		 version=${release[1]//.}
+		 version=${version:0:4}
 	fi
 	echo $version
 }
