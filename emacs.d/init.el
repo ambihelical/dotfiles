d0a7b7c2926a223c5617447029805bd82258407b
ambihelical <ambihelical@users.noreply.github.com>
2015-12-21 23:02:27 -0800

    [emacs] improve compilation mode
    * keys to move to next/previous error and file
    * turn on follow minor mode
    * fix going to first error

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -38,7 +38,22 @@
 (mouse-avoidance-mode 'animate)                             ; move mouse pointer out of way
 (column-number-mode t)                                      ; display column/row of cursor in mode-line
 (display-time-mode t)                                       ; display time in mode-line
-(setq compilation-scroll-output "first-error")
+
+;; compilation
+(setq compilation-scroll-output 'first-error)
+(setq compilation-auto-jump-to-first-error t)
+(add-hook 'compilation-mode-hook
+	(lambda ()
+		(next-error-follow-minor-mode t)
+		(local-set-key (kbd "k") 'compilation-previous-error)
+		(local-set-key (kbd "j") 'compilation-next-error)
+		(local-set-key (kbd "C-k") 'compilation-previous-file)
+		(local-set-key (kbd "C-j") 'compilation-next-file)
+		(local-set-key (kbd "<prior>") 'compilation-previous-error)
+		(local-set-key (kbd "<next>") 'compilation-next-error)
+		(local-set-key (kbd "<home>") 'compilation-previous-file)
+		(local-set-key (kbd "<end>") 'compilation-next-file)))
+
 
 (defun replace-prefix (prefix input)
 	(replace-regexp-in-string ( concat "^" (regexp-quote prefix)) "" input))
