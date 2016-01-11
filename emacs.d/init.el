cc81491c9c78aed09078d7d297aaa3eff69ca83d
ambihelical <ambihelical@users.noreply.github.com>
2016-01-10 18:05:42 -0800

    [emacs, scripts] Add irony-mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -362,6 +362,24 @@
   :config
   :diminish company-mode)
 
+; N.B. to use, need to run irony-install-server, which requires libclang-dev
+(use-package irony
+  :init
+  (progn
+    (add-hook 'c++-mode-hook 'irony-mode)
+    (add-hook 'c-mode-hook 'irony-mode)
+    (add-hook 'objc-mode-hook 'irony-mode)
+    ;; replace the `completion-at-point' and `complete-symbol' bindings in
+    ;; irony-mode's buffers by irony-mode's function
+    (defun me:irony-mode-hook ()
+      (define-key irony-mode-map [remap completion-at-point]
+        'irony-completion-at-point-async)
+      (define-key irony-mode-map [remap complete-symbol]
+        'irony-completion-at-point-async))
+    (add-hook 'irony-mode-hook 'me:irony-mode-hook)
+    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)))
+
+
 ; enable code folding (evil has bindings)
 (use-package hideshow
   :init
