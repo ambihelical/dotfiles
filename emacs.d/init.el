eb84df71b4455530349810291de05496728930c3
ambihelical <ambihelical@users.noreply.github.com>
2016-10-08 13:56:02 -0700

    [emacs] Package up my extra commands

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -51,38 +51,8 @@
   (interactive)
   (save-some-buffers t))
 
-;; select the nth other buffer
-(defun me:select-nth-other-buffer (arg)
-  (interactive "p")
-  (let ((buffer (nth arg (-filter 'buffer-file-name (buffer-list)))))
-    (if buffer
-        (switch-to-buffer buffer))))
-
-;; rotate the current fill-column
-(defun me:rotate-fill-column ()
-  (interactive)
-  (setq fill-column (cond ((< fill-column 120) (+ fill-column 10))
-                          ((>= fill-column 120) 70)))
-  (whitespace-mode -1)
-  (whitespace-mode))
-
-(defconst me:powerline-separators #'( alternate arrow arrow-fade bar box brace butt chamfer contour curve rounded roundstub wave zigzag utf-8))
-(defvar me:current-powerline-separator-index 12)               ; current powerline separator index
-
-;; rotate the powerline seperator style
-(defun me:next-powerline-separator ()
-  (interactive)
-  (if (not me:current-powerline-separator-index)
-      (setq me:current-powerline-separator-index 0))
-  (setq me:current-powerline-separator-index (% (+ me:current-powerline-separator-index 1) (length me:powerline-separators)))
-  (setq powerline-default-separator (nth me:current-powerline-separator-index me:powerline-separators))
-  (message "%s" powerline-default-separator)
-  (spaceline-compile))
-
-
 ;; configuration I haven't figured out how to wedge into
 ;; use-package
-
 
 (setq-default tab-width 3                                   ; ideal tab width
               indent-tabs-mode t                            ; enable tabs for most files
@@ -146,6 +116,11 @@
 (global-unset-key (kbd "<f10>"))                            ; was menu-bar-open
 (global-unset-key (kbd "<f11>"))                            ; was fullscreen mode
 
+;; some demand loaded commands I wrote
+(use-package me:extras
+  :ensure nil
+  :commands (me:select-nth-other-buffer me:rotate-fill-column me:next-powerline-separator)
+  :load-path "lisp/")
 
 ;; keymappings
 (use-package general
---
+++ emacs.d/lisp/me:extras.el
@@ -0,0 +1,38 @@
+;;; Commentary:
+
+;; Miscellaneous commands which can be demand loaded
+
+;;; Code:
+
+;;;###autoload
+(defun me:select-nth-other-buffer (arg)
+  "Select the nth other buffer"
+  (interactive "p")
+  (let ((buffer (nth arg (-filter 'buffer-file-name (buffer-list)))))
+    (if buffer
+        (switch-to-buffer buffer))))
+
+;;;###autoload
+(defun me:rotate-fill-column ()
+  "Rotate the current fill-column between 70 and 120 incrementing by 10"
+  (interactive)
+  (setq fill-column (cond ((< fill-column 120) (+ fill-column 10))
+                          ((>= fill-column 120) 70)))
+  (whitespace-mode -1)
+  (whitespace-mode))
+
+(defconst me:powerline-separators #'( alternate arrow arrow-fade bar box brace butt chamfer contour curve rounded roundstub wave zigzag utf-8))
+(defvar me:current-powerline-separator-index 12)               ; current powerline separator index
+
+;;;###autoload
+(defun me:next-powerline-separator ()
+  "Rotate the powerline seperator style"
+  (interactive)
+  (if (not me:current-powerline-separator-index)
+      (setq me:current-powerline-separator-index 0))
+  (setq me:current-powerline-separator-index (% (+ me:current-powerline-separator-index 1) (length me:powerline-separators)))
+  (setq powerline-default-separator (nth me:current-powerline-separator-index me:powerline-separators))
+  (message "%s" powerline-default-separator)
+  (spaceline-compile))
+
+(provide 'me:extras)
