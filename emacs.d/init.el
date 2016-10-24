c0dca8151c3541ccd6f440540130e3b736be7bf2
ambihelical <ambihelical@users.noreply.github.com>
2016-10-24 12:08:42 -0700

    [emacs] fix compilation key bindings
    Using local-set-key interfered with derived modes, this works better
    with ivy-occur for example.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -731,16 +731,16 @@
               (lambda (proc) (compilation-set-skip-threshold 2)))
     (add-hook 'compilation-mode-hook
       (lambda ()
-        (local-set-key (kbd "<tab>") #'me:rotate-skip-threshold)
-        (local-set-key (kbd "f") #'next-error-follow-minor-mode)
-        (local-set-key (kbd "k") #'compilation-previous-error)
-        (local-set-key (kbd "j") #'compilation-next-error)
-        (local-set-key (kbd "C-k") #'compilation-previous-file)
-        (local-set-key (kbd "C-j") #'compilation-next-file)
-        (local-set-key (kbd "<prior>") #'compilation-previous-error)
-        (local-set-key (kbd "<next>") #'compilation-next-error)
-        (local-set-key (kbd "<home>") #'compilation-previous-file)
-        (local-set-key (kbd "<end>") #'compilation-next-file)))))
+        (define-key compilation-mode-map (kbd "<tab>") #'me:rotate-skip-threshold)
+        (define-key compilation-mode-map (kbd "f") #'next-error-follow-minor-mode)
+        (define-key compilation-mode-map (kbd "k") #'compilation-previous-error)
+        (define-key compilation-mode-map (kbd "j") #'compilation-next-error)
+        (define-key compilation-mode-map (kbd "C-k") #'compilation-previous-file)
+        (define-key compilation-mode-map (kbd "C-j") #'compilation-next-file)
+        (define-key compilation-mode-map (kbd "<prior>") #'compilation-previous-error)
+        (define-key compilation-mode-map (kbd "<next>") #'compilation-next-error)
+        (define-key compilation-mode-map (kbd "<home>") #'compilation-previous-file)
+        (define-key compilation-mode-map (kbd "<end>") #'compilation-next-file)))))
 
 (use-package doc-view
   :ensure nil
