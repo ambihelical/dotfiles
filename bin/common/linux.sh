92624f3d7bdd2873b849b6ae98a153d6b4705cf8
ambihelical <ambihelical@users.noreply.github.com>
2016-12-07 09:50:35 -0800

    [bash] use local variables
    No functional changes

--- bin/common/linux.sh
+++ bin/common/linux.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+# keep above line to select correct syntax in editor
 
 # return the name of the current linux distribution
 linux_distribution_name() {
-	distro="Unknown"
+	local distro="Unknown"
 	if hash lsb_release 2> /dev/null; then
-		 release=( $(lsb_release -ds) )
+		 local release=( $(lsb_release -ds) )
 		 distro=${release[0]}
 	fi
 	echo $distro
@@ -14,9 +16,9 @@ linux_distribution_name() {
 # Other distributions may not be so readable if the numbers
 # must stay comparable.
 linux_distribution_version() {
-	version="0000"
+	local version="0000"
 	if hash lsb_release 2> /dev/null; then
-		 release=( $(lsb_release -ds) )
+		 local release=( $(lsb_release -ds) )
 		 version=${release[1]//.}
 		 version=${version:0:4}
 	fi
