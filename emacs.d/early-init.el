2c3fcb7557d0f3342b6071125d71d59f483ba96c
ambihelical <ambihelical@users.noreply.github.com>
2020-08-23 18:43:24 -0700

    [emacs] disable file-handlers during startup

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -1,4 +1,18 @@
 ;;; -*- lexical-binding: t; -*-
 
+;; disable GC at startup, this is fixed later by gcmh
 (setq gc-cons-threshold most-positive-fixnum)
+
+;; use quickstart feature for 10% faster startup
 (setq package-quickstart t)
+
+;; ?
+;;(setq package-enable-at-startup nil)
+
+;; disable file-handlers during startup for another 10% saving
+(defvar me:file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq file-name-handler-alist me:file-name-handler-alist)
+            (makunbound 'me:file-name-handler-alist)))
