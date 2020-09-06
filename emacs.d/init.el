b1dc738c816b0f1f226a17140351bc03c0d89319
ambihelical <ambihelical@users.noreply.github.com>
2020-09-06 13:02:45 -0700

    [emacs] Turn off eglot for C++
    The only C++ code I edit is remote nowadays, and eglot doesn't
    work yet remotely, and really never really worked well for the project
    I do work on, so disabling it for C++ for now.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1571,9 +1571,7 @@
   (:keymaps 'eglot-mode-map
             "<f6> c"  #'eglot-code-actions
             "<f6> h"  #'eglot-help-at-point)
-  :hook ((c-mode . eglot-ensure)
-         (rust-mode . eglot-ensure)
-         (c++-mode . eglot-ensure))
+  :hook ((rust-mode . eglot-ensure))
   :init
   (setq eglot-ignored-server-capabilites '( :documentHighlightProvider)
         eglot-send-changes-idle-time 3    ;; be slower sending changes
