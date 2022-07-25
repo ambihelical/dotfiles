e00c0f39195ae2187741ace4fa15852e53d3c80c
ambihelical <ambihelical@users.noreply.github.com>
2022-07-25 10:28:59 -0700

    [emacs] Load company
    Something doesn't work right when company is not loaded,
    completions will take out blocks of text.  This seems
    to fix it, but not sure why.

--- emacs/init.el
+++ emacs/init.el
@@ -1419,6 +1419,8 @@
                    `((c++-mode c-mode) "ccls" ,init-str))))
   :hook ((rust-mode c++-mode c-mode) . eglot-ensure))
 
+(use-package company :demand t)
+
 (use-package corfu
   :demand t
   :after vertico
