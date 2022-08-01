2dbd965ec6703583dc33d1224da28b4cf4e015bc
ambihelical <ambihelical@users.noreply.github.com>
2022-07-31 21:55:53 -0700

    [emacs] Hopefully fix package-initialized messages

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -8,7 +8,8 @@
       package-quickstart t                      ; 10% faster, I'll take it
       file-name-handler-alist nil               ; faster startup without this
       frame-resize-pixelwise t                  ; disable rounding of frame size
-      package-enable-at-startup nil)            ; ?
+      package--init-file-ensured t              ;
+      package-enable-at-startup nil)            ; don't make packages available at start
 
 (add-hook 'emacs-startup-hook
           (lambda ()
