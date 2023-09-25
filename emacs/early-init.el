59cad3682a3712b25b2010fbf82505f4b515d767
ambihelical <ambihelical@users.noreply.github.com>
2023-09-24 18:28:27 -0700

    [emacs] Remove orgmode.org/elpa package archive it's deprecated

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -31,7 +31,6 @@
 
 ;; package management
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org"   . "https://orgmode.org/elpa/")
                          ("gnu"   . "https://elpa.gnu.org/packages/")
                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 (require 'package)
