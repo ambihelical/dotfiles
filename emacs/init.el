32f72479259b5ea46cd99a1b6cb37650ff7a7a95
ambihelical <ambihelical@users.noreply.github.com>
2024-08-11 14:26:51 -0700

    [emacs] Add goto-addr package
    Open web-links almost everywhere

--- emacs/init.el
+++ emacs/init.el
@@ -930,6 +930,18 @@
     (setq define-word-limit (/ (+ 2 (frame-height)) 3)))
   (advice-add #'define-word :before #'me:advise-define-word))
 
+;; Make web addresses actionable
+(use-package goto-addr
+  :hook ((compilation-mode . goto-address-mode)
+         (prog-mode . goto-address-prog-mode)
+         (eshell-mode . goto-address-mode)
+         (shell-mode . goto-address-mode))
+  :bind (:map goto-address-highlight-keymap
+              ("<RET>" . goto-address-at-point)
+              ("M-<RET>" . newline))
+  :commands (goto-address-prog-mode
+             goto-address-mode))
+
 ;; vertical completion
 (use-package vertico
   :general
