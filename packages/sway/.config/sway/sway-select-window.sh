a12115ebf1e9e47df1394c7180999b4cc8ec81cc
ambihelical <ambihelical@users.noreply.github.com>
2026-02-16 22:04:56 -0800

    [sway] Getting stuff to work again
    * add waybar back
    * add gnome-calendar
    * make setup-dotfiles omnipotent

--- packages/sway/.config/sway/sway-select-window.sh
+++ packages/sway/.config/sway/sway-select-window.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-window_id=$(swaymsg -t get_tree | jq -r '.. | select(.type? == "con" and .name) | .id as $id | "\(.name) (\($id))"' | rofi -dmenu -i -no-lazy-grab -p "Select window" | grep -oP '\(\K[^\)]*')
+window_id=$(swaymsg -t get_tree | jq -r '.. | select(.type? == "con" and .name) | .id as $id | "\(.name) (\($id))"' | wofi --dmenu --insensitive --prompt="Select window" | grep -oP '\(\K[^\)]*')
 if [[ -n $window_id ]]; then
   swaymsg "[con_id=$window_id]" focus
 fi
